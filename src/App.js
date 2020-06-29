55183ab7238d976158b40fbb98e727fa04e97d69
juniordugue <jrdugue@gmail.com>
2020-06-29 14:10:25 -0400

    ran successful tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+// import React, { Component } from "react";
 import React from "react";
-import "./styles.css";
+// import "./styles.css";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App" data-test="component-app">
+    <h2>App</h2>
     </div>
   );
 }
+
+export default App;
