b20a03ab7e149b68a15a2382ef34f7a7af419f22
Junior Dugue <jrdugue@gmail.com>
2020-06-29 11:56:54 -0400

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {});
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
